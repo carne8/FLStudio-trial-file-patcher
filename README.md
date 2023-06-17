36956cc65f3c94e71d173d894366cc663dbf26bb
Gaël Bouquain <gael.bouquain@gmail.com>
2023-06-17 22:15:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# FLStudio trial file patcher
+
+This program will allow you to re-open a file you created with FLStudio. **It work only for FLStudio 20**
+
+# Usage
+1. Download the program [here](https://github.com/carne8/flstudio-trial-file-patcher/releases/latest)
+1. Run the program in the cmd with the path of the FLStudio file as first argument
+1. Open the file in FLStudio
---
+++ src/Program.fs
@@ -0,0 +1,38 @@
+﻿open System.IO
+
+let updateFile path = task {
+    let! bytes = File.ReadAllBytesAsync path
+    let mutable updated = false
+
+    let newBytes =
+        bytes
+        |> Array.pairwise
+        |> Array.map (fun (b1, b2) ->
+            if not updated && b1 = 28uy then
+                updated <- true
+                1uy
+            else
+                b2
+        )
+        |> Array.append [| bytes |> Array.head |]
+
+    do! File.WriteAllBytesAsync (path, newBytes)
+}
+
+[<EntryPoint>]
+let main args =
+    match args |> Array.tryItem 0 with
+    | Some filePath ->
+        match File.Exists filePath with
+        | false ->
+            failwith "File doesn't exist"
+            2
+        | true ->
+            printfn "Patching file ..."
+            updateFile(filePath).Result
+            printfn "File patched ! ⭐"
+
+            0
+    | None ->
+        failwith "No path in arguments"
+        1
